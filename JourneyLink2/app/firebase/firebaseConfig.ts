5c4452859fd799432e38897efbeaffde00a65d6c
Anski Saint-Fleur <anskistfleur@gmail.com>
2024-09-21 17:03:56 -0400

    updated db config changes

--- JourneyLink2/app/firebase/firebaseConfig.ts
+++ JourneyLink2/app/firebase/firebaseConfig.ts
@@ -1,21 +1,38 @@
-// /firebase/firebaseConfig.ts
 import { initializeApp } from 'firebase/app';
 import { getAuth, Auth } from 'firebase/auth';
-import { getAnalytics } from 'firebase/analytics';
+import { getFirestore, Firestore } from 'firebase/firestore';
+import { getAnalytics, isSupported } from 'firebase/analytics';
 
 // Your Firebase configuration from Firebase console
 const firebaseConfig = {
-    apiKey: "AIzaSyC_tztX19mBA_kyHHGitsPex2eh856UruY",
-    authDomain: "journeylink-60cac.firebaseapp.com",
-    projectId: "journeylink-60cac",
-    storageBucket: "journeylink-60cac.appspot.com",
-    messagingSenderId: "259673028525",
-    appId: "1:259673028525:web:e51f72430e0de258485324",
-    measurementId: "G-54HWNK052C"
-  };
+  apiKey: "AIzaSyC_tztX19mBA_kyHHGitsPex2eh856UruY",
+  authDomain: "journeylink-60cac.firebaseapp.com",
+  projectId: "journeylink-60cac",
+  storageBucket: "journeylink-60cac.appspot.com",
+  messagingSenderId: "259673028525",
+  appId: "1:259673028525:web:e51f72430e0de258485324",
+  measurementId: "G-54HWNK052C"
+};
 
-// Initialize Firebase app and auth
+// Initialize Firebase app
 const app = initializeApp(firebaseConfig);
+
+// Initialize Firebase Auth
 const auth: Auth = getAuth(app);
-const analytics = getAnalytics(app);
-export { auth };
+
+// Initialize Firestore
+const db: Firestore = getFirestore(app);
+
+// Conditionally initialize Firebase Analytics
+isSupported().then((supported) => {
+  if (supported) {
+    const analytics = getAnalytics(app);
+    console.log('Firebase Analytics initialized');
+  } else {
+    console.log('Firebase Analytics is not supported in this environment.');
+  }
+}).catch((error) => {
+  console.error('Error checking analytics support:', error);
+});
+
+export { auth, db };
